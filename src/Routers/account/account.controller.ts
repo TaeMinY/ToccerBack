4ea4f58ff55ff16a638a37acdbeaa5c66e7c1537
UniTaeMin <onnu0802@naver.com>
2020-02-22 01:07:30 +0900

    Add SignUp Controller
    Add SignUp Controller

--- src/Routers/account/account.controller.ts
+++ src/Routers/account/account.controller.ts
@@ -6,45 +6,65 @@ import * as nodemailer from "nodemailer"
 import * as path from "path"
 import * as jwt from "jsonwebtoken"
 import * as shortid from "shortid"
+const idRule = /^.*(?=^.{4,15}$)(?=.*\d)(?=.*[a-zA-Z]).*$/
 const passwordRule = /^.*(?=^.{6,15}$)(?=.*\d)(?=.*[a-zA-Z]).*$/
 const emailRule = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i
 require("dotenv").config()
 
 export const Signup = async (req: Request, res: Response) => {
-  const { username, password, password2, email } = req.body
-  if (!username || !password || !password2 || !email) {
+  const { id, pwd, pwd2, username, email, profile__expansion, terms } = req.body
+  if (terms == false) {
+    return Send(res, 200, "약관을 동의해주세요.", false)
+  }
+  if (!id || !pwd || !pwd2 || !username || !email) {
     return Send(res, 200, "빈칸을 모두 입력해 주세요.", false)
+  }
+  if (!idRule.test(id)) {
+    return Send(res, 200, "아이디는 영문과 숫자를 포함하여 4~15자리로 입력해주세요.", false)
+  }
+  if (!(2 <= username.length || username.length <= 12)) {
+    return Send(res, 200, "닉네임은 4~12자리로 입력해주세요.", false)
   }
   if (!emailRule.test(email)) {
     return Send(res, 200, "올바른 이메일 형식이 아닙니다.", false)
   }
-  if (password != password2) {
-    return Send(res, 200, "비밀번호가 일치하지 않습니다.", false)
-  }
-  if (!passwordRule.test(password)) {
+  if (!passwordRule.test(pwd)) {
     return Send(res, 200, "비밀번호는 영문과 숫자를 포함하여 6~15자리로 입력해주세요.", false)
   }
+  if (pwd != pwd2) {
+    return Send(res, 200, "비밀번호가 일치하지 않습니다.", false)
+  }
 
-  User.findOne({ email: email }, async function(err, result) {
+  User.findOne({ id: id }, async function(err, result) {
     if (err) throw err
     if (result == null) {
-      //생성
-      bcrypt.hash(password, null, null, async function(err, hash) {
-        const user: any = new User({
-          email: email,
-          password: hash,
-          username: username,
-          profile_image: await shortid.generate()
-        })
-        await user
-          .save()
-          .then(data => {
-            return Send(res, 200, "회원가입에 성공했습니다!", true)
+      User.findOne({ email: email }, async function(err, result) {
+        if (result == null) {
+          bcrypt.hash(pwd, null, null, async function(err, hash) {
+            const random = await shortid.generate()
+            const user: any = new User({
+              id: id,
+              password: hash,
+              username: username,
+              email: email,
+              profile: random + "." + profile__expansion
+            })
+            await user
+              .save()
+              .then(data => {
+                return res
+                  .status(200)
+                  .send({ state: true, result: "회원가입이 되셨습니다.", profileText: random })
+                  .end()
+              })
+              .catch(err => Send(res, 200, "DB 저장을 실패했습니다.", false))
           })
-          .catch(err => Send(res, 200, "DB 저장을 실패했습니다.", false))
+        } else {
+          return Send(res, 200, "이미 존재하는 이메일입니다.", false)
+        }
       })
     } else {
-      return Send(res, 200, "이미 존재하는 계정입니다.", false)
+      return Send(res, 200, "이미 존재하는 아이디입니다.", false)
     }
   })
 }
