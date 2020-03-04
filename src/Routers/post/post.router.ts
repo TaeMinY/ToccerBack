e01ed7a810fc68f22828f0978f5ce32307968f3b
UniTaeMin <onnu0802@naver.com>
2020-03-05 04:43:30 +0900

    Add Dev Post Init
    Add Dev Post Init

--- src/Routers/post/post.router.ts
+++ src/Routers/post/post.router.ts
@@ -1,7 +1,8 @@
 import * as express from "express"
-import { Create, FindAll } from "./post.controller"
+import { Create, FindAll, Init } from "./post.controller"
 const router = express.Router()
 
 router.post("/create", Create)
 router.post("/findall", FindAll)
+router.get("/init", Init)
 export default router
