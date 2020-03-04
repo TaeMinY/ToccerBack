c64689ccb4567e1f97b077d1a6a08bc73d99bf7a
UniTaeMin <onnu0802@naver.com>
2020-03-04 19:46:07 +0900

    Add FindAll Router
    Add FindAll Router

--- src/Routers/post/post.router.ts
+++ src/Routers/post/post.router.ts
@@ -1,7 +1,7 @@
 import * as express from "express"
-import { Create } from "./post.controller"
+import { Create, FindAll } from "./post.controller"
 const router = express.Router()
 
 router.post("/create", Create)
-
+router.post("/findall", FindAll)
 export default router
