38785ab7c28f8298e672de60c6041016844176ab
UniTaeMin <onnu0802@naver.com>
2020-03-05 08:43:56 +0900

    Add Comment Controller
    Add Comment Controller

---
+++ src/Routers/comment/comment.controller.ts
@@ -0,0 +1,40 @@
+import * as bcrypt from "bcrypt-nodejs"
+import { Request, Response } from "express"
+import Comment from "../../Model/comment"
+
+import User from "../../Model/account"
+import * as moment from "moment"
+import Send from "../../Module/Send"
+import * as jwt from "jsonwebtoken"
+
+require("dotenv").config()
+
+export const Create = async (req: Request, res: Response) => {
+  const { post_id, text, token } = req.body
+  if (!text) {
+    return Send(res, 200, "댓글을 입력해 주세요.", false)
+  }
+  let decoded = jwt.verify(token, process.env.jwtpassword)
+
+  User.findOne({ id: decoded.id }, async function(err, result) {
+    if (result != null) {
+      const comment: any = new Comment({
+        post_id: post_id,
+        text: text,
+        time: moment().format("YYYY-MM-DD-HH-mm-ss"),
+        username: result.username
+      })
+      await comment
+        .save()
+        .then(data => {
+          return res
+            .status(200)
+            .send({ state: true, result: "댓글을 정상적으로 등록하였습니다." })
+            .end()
+        })
+        .catch(err => Send(res, 200, "DB 저장을 실패했습니다.", false))
+    } else {
+      return Send(res, 200, "댓글을 작성하지 못하였습니다.", false)
+    }
+  })
+}
