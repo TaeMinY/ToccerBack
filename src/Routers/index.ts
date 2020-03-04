7aef95aef6882e25c34f0cd306b8eb862174cbf1
UniTaeMin <onnu0802@naver.com>
2020-03-05 08:44:13 +0900

    Update Router
    Update Router

--- src/Routers/index.ts
+++ src/Routers/index.ts
@@ -1,7 +1,9 @@
 import * as express from "express"
 import Account from "./account/account.router"
 import Post from "./post/post.router"
-const router = express.Router();
+import Comment from "./comment/comment.router"
+const router = express.Router()
 router.use("/account", Account)
-router.use("/post",Post)
+router.use("/post", Post)
+router.use("/comment", Comment)
 export default router
