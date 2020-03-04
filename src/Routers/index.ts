6bea3a0b9075f98102e8bac1b72307842b998bef
UniTaeMin <onnu0802@naver.com>
2020-03-04 16:43:48 +0900

    Add Post Router
    Add Post Router

--- src/Routers/index.ts
+++ src/Routers/index.ts
@@ -1,7 +1,7 @@
 import * as express from "express"
 import Account from "./account/account.router"
-
+import Post from "./post/post.router"
 const router = express.Router();
 router.use("/account", Account)
-
+router.use("/post",Post)
 export default router
